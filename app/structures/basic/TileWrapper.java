325675317eb8bb0583dabd0f5e064296397ca5b9
Christy, Darby Lynn <2860148c@student.gla.ac.uk>
2024-02-13 18:52:07 +0000

    This is for the Board and TileWrapper class

---
+++ app/structures/basic/TileWrapper.java
@@ -0,0 +1,64 @@
+package structures.basic;
+
+public class TileWrapper {
+	
+	private Tile tile; 
+	private UnitWrapper unit;
+	private boolean hasUnit; 
+	private int xpos; 
+	private int ypos;
+	
+	public TileWrapper(int xpos, int ypos) {
+		this.xpos = xpos;
+		this.ypos = ypos;
+		this.hasUnit = false;
+	}
+
+	public boolean getHasUnit() {
+		return hasUnit;
+	}
+
+	public void setHasUnit(boolean hasUnit) {
+		this.hasUnit = hasUnit;
+	}
+
+	public int getXpos() {
+		return xpos;
+	}
+
+	public void setXpos(int xpos) {
+		this.xpos = xpos;
+	}
+
+	public int getYpos() {
+		return ypos;
+	}
+
+	public void setYpos(int ypos) {
+		this.ypos = ypos;
+	}
+
+	public Tile getTile() {
+		return tile;
+	}
+
+	public void setTile(Tile tile) {
+		this.tile = tile;
+	}
+	
+	public UnitWrapper getUnit() {
+		return unit;
+	}
+
+	public void setUnitWrapper(UnitWrapper unit) {
+		this.unit = unit;
+	}
+	
+	
+
+	
+	
+	
+	
+
+}
